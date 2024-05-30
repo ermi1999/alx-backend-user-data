b03d2d6102cf8574f114c8bd3dc02e78cf3b9051
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-30 21:55:17 +0300

    password hashing

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
 """This module encrypts a password"""
-import 
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """this function hashes a password."""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
