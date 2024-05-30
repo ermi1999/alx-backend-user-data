372175904331007a93037aa620adc3309309cd7a
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-30 17:00:00 +0300

    connect to mysql with environment variables

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,9 @@
 import re
 from typing import List
 import logging
+import mysql
+import mysql.connector
+import os
 
 
 class RedactingFormatter(logging.Formatter):
@@ -50,3 +53,13 @@ def get_logger() -> logging.Logger:
 
     logger.addHandler(target_handler)
     return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """connects to mysql database"""
+    return mysql.connector.connect(
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''),
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME')
+    )
