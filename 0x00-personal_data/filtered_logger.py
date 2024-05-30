804fedcb5e543cb502dda1b1447ae32308c2cae5
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-30 15:21:26 +0300

    initial commit

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """module for filtering and replacing a string"""
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """function for filtering and replacing a string using regex"""
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
