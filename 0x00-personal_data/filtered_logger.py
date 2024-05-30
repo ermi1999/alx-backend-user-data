0a1a4799bab31b93d32ffd6c54e4b6b961e0e948
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-30 15:48:17 +0300

    formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,15 +5,6 @@ from typing import List
 import logging
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    """function for filtering and replacing a string using regex"""
-    for field in fields:
-        message = re.sub(f'{field}=(.*?){separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
-
-
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
         """
@@ -31,3 +22,12 @@ class RedactingFormatter(logging.Formatter):
         """Formatter"""
         return filter_datum(self.fields, self.REDACTION,
                             super().format(record), self.SEPARATOR)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """function for filtering and replacing a string using regex"""
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
