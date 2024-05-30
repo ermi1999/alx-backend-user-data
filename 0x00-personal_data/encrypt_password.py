7220132be83b8efe956da76c2af8ccf342c67bb6
Ermiyas <ermiyasabiye1999@gmail.com>
2024-05-30 22:00:31 +0300

    is valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,3 +6,8 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """this function hashes a password."""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if the hashed password is endeed the password."""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
