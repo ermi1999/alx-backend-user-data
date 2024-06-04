5969e34f306eceb16a01f139495471374b1f27b8
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-04 20:10:20 +0000

    auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""
+Auth
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """A class for handling authentication.
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """to be documented
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """authorization header
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """current user
+        """
+        return None
