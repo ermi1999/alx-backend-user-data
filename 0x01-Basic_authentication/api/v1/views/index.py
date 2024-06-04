e1186710ac527f59d43ae5dda075410103128e10
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-04 19:52:48 +0000

    unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,14 +5,6 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route("/unauthorized", strict_slashes=False)
-def status() -> None:
-    """
-    error handler
-    """
-    abort(401)
-
-
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
@@ -32,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route("/unauthorized/", strict_slashes=False)
+def anauthorized() -> None:
+    """
+    error handler
+    """
+    abort(401)
