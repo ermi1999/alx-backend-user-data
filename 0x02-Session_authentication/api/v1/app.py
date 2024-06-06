bac01ce1d3ee8af8b767028b5da3548282e44e30
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-06 22:32:25 +0300

    before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -36,7 +36,12 @@ def before():
     if not auth:
         return
     request.current_user = auth.current_user(request)
-    paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    paths = [
+        '/api/v1/status/', '/api/v1/unauthorized/',
+        '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+    if (auth.authorization_header(request) is None
+       or auth.session_cookie(request) is None):
+        abort(404)
     if auth.require_auth(request.path, paths):
         if auth.authorization_header(request) is None:
             abort(401)
