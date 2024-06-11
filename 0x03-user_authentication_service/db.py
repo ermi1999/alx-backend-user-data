ed8247b54af1afbbc4c25c3887b8b4f188118958
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 06:16:58 +0000

    add user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -32,13 +32,13 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User
+    def add_user(self, email: str, hashed_password: str) -> User:
         """creates a user"""
+        user = User(email=email, hashed_password=hashed_password)
         try:
-            user = User(email=email, hashed_password=hashed_password)
             self._session.add(user)
             self._session.commit()
         except Exception:
             self._session.rollback()
-            user = None
+            raise
         return user
