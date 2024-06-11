dd83e6952a61a2664b2b05b911f870304d626727
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 23:00:54 +0300

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -83,7 +83,7 @@ class Auth:
         """updates the user password given a correct reset_token"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            hashed_password = _hash_password(password).decode('utf-8')
+            hashed_password = _hash_password(password)
             self._db.update_user(
                     user.id, hashed_password=hashed_password, reset_token=None)
         except NoResultFound:
