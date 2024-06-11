1d9d3b3178e93410d45aebd2dac0a73aea3fbb62
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 22:50:38 +0300

    update password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -78,3 +78,15 @@ class Auth:
             return reset_token
         except Exception:
             raise ValueError
+    
+    def update_password(self, reset_token: str, password: str) -> None:
+        """resets password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            hashed = _hash_password(password)
+            self._db.update_user(
+                user.id, hashed_password=hashed.decode('utf-8'),
+                reset_token=None
+                )
+        except Exception:
+            raise ValueError
