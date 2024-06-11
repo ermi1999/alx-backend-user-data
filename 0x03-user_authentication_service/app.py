39a2a4270403d29826f9b7f115a8c5a623725c0c
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 14:45:23 +0300

    set session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,5 +26,17 @@ def users():
         return jsonify({"message": "email already registered"})
 
 
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def sessions():
+    """creates a new session for user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        res = jsonify({"email": email, "message": "logged in"})
+        res.set_cookie("session_id", session_id)
+        return res
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
