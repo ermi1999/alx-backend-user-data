5d795cc84b00ca64ce4e14344aa3eb746066688f
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 13:32:12 +0300

    credential validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -34,8 +34,7 @@ class Auth:
         """valid login"""
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode('utf-8'), user.hashed_password):
-                return True
-            return False
+            return bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password.encode('utf-8'))
         except Exception:
             return False
