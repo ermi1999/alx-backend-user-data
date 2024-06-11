63b27beb4a2f6bb5200dc30bc10435285e0d2dac
Ermiyas <ermiyasabiye1999@gmail.com>
2024-06-11 22:32:24 +0300

    user profile

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -50,5 +50,14 @@ def del_sessions() -> str:
     abort(403)
 
 
+@app.route('/profile', strict_slashes=False)
+def get_profile() -> str:
+    """gets a profile with session id"""
+    user = AUTH.get_user_from_session_id(request.cookies.get('session_id'))
+    if user:
+        return jsonify({"email": user.email}), 200
+    abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
